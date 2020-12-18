824ff9e75d63d6b83c9eabfb5542390deef9f6af
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-12-18 01:51:26 -0500

    nueva ruta

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express    = require('express'),
       app        = express(),
       cors       = require('cors'),
       bodyParser = require('body-parser'),
-      port       = 3000;
+      port       = 3001;
 
 
 // Aqui pon la logica
@@ -13,16 +13,17 @@ app
 .use(bodyParser.json({ limit: '5mb' }));
 
 
-const r_login   = require('./app/routes/r_login');
-// const r_auth    = require('./app/modules/auth/r_auth');
+// const r_login   = require('./app/routes/r_login');
+const r_auth    = require('./app/modules/auth/r_auth');
+const r_imei    = require('./app/modules/imei/r_imei');
 // const r_profile = require('./app/modules/profile/r_profile');
 // const r_publication = require('./app/modules/publication/r_publication');
 
 app
-.use('/login', r_login)
+// .use('/login', r_auth)
 // .use('/profile', r_profile)
-// .use('/publication', r_publication)
-// .use('/auth', r_auth);
+.use('/imei', r_imei)
+.use('/auth', r_auth);
 
 
 app.get('/', (req, res) => {
